a43ab5199c10ad00fe83eb9ff4af627e606b4667
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-06-06 12:46:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import "./index.css";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
+import { BrowserRouter as Router } from "react-router-dom";
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = createRoot(rootElement);
+
+
+root.render(
+  <React.StrictMode>
+    <MoralisProvider
+      initializeOnMount
+      appId={"xxx"}
+      serverUrl={"xxx"}
+    >
+      <NotificationProvider>
+        <Router>
+          <App />
+        </Router>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>
+);
+
+
